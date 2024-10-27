d0c628e39c2969f58b46fda618eaaf352cbc394f
John A. De Goes <john@degoes.net>
2024-10-27 06:41:28 -0400

    scalafmt

--- src/main/scala/net/degoes/03-concurrency.scala
+++ src/main/scala/net/degoes/03-concurrency.scala
@@ -1,24 +1,21 @@
-/**
-  * CONCURRENCY
-  * 
-  * Achieving scalable concurrency is a critical feature of any functional 
-  * effect system. However, functional effect systems tend to go much 
-  * further than just making concurrency scalable--they offer safer and 
-  * more compositional concurrency primitives.
-  * 
-  * In this section, you will explore some of the foundational elements 
-  * of concurrency in effect system, learning by implementing. What you 
-  * learn will improve your understanding of concurrency, as well as 
-  * apply to any functional effect system.
+/** CONCURRENCY
+  *
+  * Achieving scalable concurrency is a critical feature of any functional effect system. However, functional effect
+  * systems tend to go much further than just making concurrency scalable--they offer safer and more compositional
+  * concurrency primitives.
+  *
+  * In this section, you will explore some of the foundational elements of concurrency in effect system, learning by
+  * implementing. What you learn will improve your understanding of concurrency, as well as apply to any functional
+  * effect system.
   */
 package net.degoes.concurrency
 
-import net.degoes.asynchronicity.Async 
-
-import zio.test._ 
+import zio.test._
 import zio.test.TestAspect.ignore
 
 import java.util.concurrent.atomic.AtomicReference
+
+import net.degoes.asynchronicity.Async
 
 object Signal:
   def make: Async[Signal] = ???
@@ -31,16 +28,14 @@ class Signal(private val atomic: AtomicReference[Option[List[(unit: Unit) => Uni
   def trigger: Async[Unit] = ???
 
 object SignalSpec extends ZIOSpecDefault:
-  def spec = 
+  def spec =
     suite("SignalSpec")(
       test("make") {
         val signal = Signal.make.block
 
-        /**
-          * EXERCISE 1
-          * 
-          * Implement the `make` method on `Signal` so that it returns an
-          * `Async` that produces a new `Signal`.
+        /** EXERCISE 1
+          *
+          * Implement the `make` method on `Signal` so that it returns an `Async` that produces a new `Signal`.
           */
         assertTrue(signal != null)
       } @@ ignore,
@@ -51,13 +46,10 @@ object SignalSpec extends ZIOSpecDefault:
 
         Async.scheduledExecutor.schedule(runnable, 10, java.util.concurrent.TimeUnit.MILLISECONDS)
 
-        /**
-          * EXERCISE 2
-          * 
-          * Implement the `await` and `trigger` methods on `Signal` so that the
-          * `await` method returns an `Async` that blocks until the signal is
-          * triggered, and the `trigger` method returns an `Async` that triggers
-          * the signal.
+        /** EXERCISE 2
+          *
+          * Implement the `await` and `trigger` methods on `Signal` so that the `await` method returns an `Async` that
+          * blocks until the signal is triggered, and the `trigger` method returns an `Async` that triggers the signal.
           */
         assertTrue(signal.await.block == ())
       } @@ ignore,
@@ -68,12 +60,10 @@ object SignalSpec extends ZIOSpecDefault:
 
         Async.scheduledExecutor.schedule(runnable, 10, java.util.concurrent.TimeUnit.MILLISECONDS)
 
-        /**
-          * EXERCISE 3
-          * 
-          * Implement the `isTriggered` method on `Signal` so that it returns
-          * an `Async` that immediately indicates whether the signal has been
-          * triggered.
+        /** EXERCISE 3
+          *
+          * Implement the `isTriggered` method on `Signal` so that it returns an `Async` that immediately indicates
+          * whether the signal has been triggered.
           */
         assertTrue(signal.isTriggered.block)
       } @@ ignore,
@@ -89,18 +79,16 @@ class Ref[A](private val atomic: AtomicReference[A]):
 
   def update(f: A => A): Async[Unit] = ???
 
-
 object RefSpec extends ZIOSpecDefault:
-  def spec = 
+  def spec =
     suite("RefSpec")(
       test("make") {
         val ref = Ref.make(42).block
 
-        /**
-          * EXERCISE 4
-          * 
-          * Implement the `make` method on `Ref` so that it returns an `Async`
-          * that produces a new `Ref` with the specified initial value.
+        /** EXERCISE 4
+          *
+          * Implement the `make` method on `Ref` so that it returns an `Async` that produces a new `Ref` with the
+          * specified initial value.
           */
         assertTrue(ref != null)
       } @@ ignore,
@@ -109,12 +97,10 @@ object RefSpec extends ZIOSpecDefault:
 
         ref.set(43).block
 
-        /**
-          * EXERCISE 5
-          * 
-          * Implement the `get` and `set` methods on `Ref` so that `get` returns
-          * an `Async` that gets the current value of the `Ref`, and `set` returns
-          * an `Async` that sets the value of the `Ref`.
+        /** EXERCISE 5
+          *
+          * Implement the `get` and `set` methods on `Ref` so that `get` returns an `Async` that gets the current value
+          * of the `Ref`, and `set` returns an `Async` that sets the value of the `Ref`.
           */
         assertTrue(ref.get.block == 43)
       } @@ ignore,
@@ -123,11 +109,10 @@ object RefSpec extends ZIOSpecDefault:
 
         ref.update(_ + 1).block
 
-        /**
-          * EXERCISE 6
-          * 
-          * Implement the `update` method on `Ref` so that it returns an `Async`
-          * that updates the value of the `Ref` using the specified function.
+        /** EXERCISE 6
+          *
+          * Implement the `update` method on `Ref` so that it returns an `Async` that updates the value of the `Ref`
+          * using the specified function.
           */
         assertTrue(ref.get.block == 43)
       } @@ ignore,
@@ -136,14 +121,13 @@ object RefSpec extends ZIOSpecDefault:
 class GreenThread[A] private ():
   def join: Async[A] = ???
 
-extension [A] (async: Async[A])
-  def fork: Async[GreenThread[A]] = ???
+extension [A](async: Async[A]) def fork: Async[GreenThread[A]] = ???
 
 object GreenThreadSpec extends ZIOSpecDefault:
-  def spec = 
+  def spec =
     suite("GreenThreadSpec")(
       test("fork + join") {
-        def par[A, B](a: Async[A], b: Async[B]): Async[(A, B)] = 
+        def par[A, B](a: Async[A], b: Async[B]): Async[(A, B)] =
           for {
             ga <- a.fork
             gb <- b.fork
@@ -156,14 +140,12 @@ object GreenThreadSpec extends ZIOSpecDefault:
 
         val result = par(a, b).block
 
-        /**
-          * EXERCISE 7
-          * 
-          * Implement the `fork` and `join` methods on `Async` so that the
-          * following test passes.
+        /** EXERCISE 7
+          *
+          * Implement the `fork` and `join` methods on `Async` so that the following test passes.
           */
         assertTrue(result == (42, "Hello"))
-      } @@ ignore,
+      } @@ ignore
     )
 
 object Promise:
@@ -175,29 +157,25 @@ class Promise[A] private (signal: Signal, ref: Ref[Option[A]]):
   def complete(a: A): Async[Unit] = ???
 
 object PromiseSpec extends ZIOSpecDefault:
-  def spec = 
+  def spec =
     suite("PromiseSpec")(
       test("make") {
         val promise = Promise.make[Int].block
 
-        /**
-          * EXERCISE 8
-          * 
-          * Implement the `make` method on `Promise` so that it returns an
-          * `Async` that produces a new `Promise`.
+        /** EXERCISE 8
+          *
+          * Implement the `make` method on `Promise` so that it returns an `Async` that produces a new `Promise`.
           */
         assertTrue(promise != null)
       } @@ ignore,
       test("await + complete") {
         val promise = Promise.make[Int].block
 
-        /**
-          * EXERCISE 9
-          * 
-          * Implement the `await` and `complete` methods on `Promise` so that
-          * the `await` method returns an `Async` that blocks until the promise
-          * is completed, and the `complete` method returns an `Async` that
-          * completes the promise.
+        /** EXERCISE 9
+          *
+          * Implement the `await` and `complete` methods on `Promise` so that the `await` method returns an `Async` that
+          * blocks until the promise is completed, and the `complete` method returns an `Async` that completes the
+          * promise.
           */
         assertTrue(promise.complete(42).fork.flatMap(_ => promise.await).block == 42)
       } @@ ignore,
@@ -212,29 +190,24 @@ class Queue[A] private ():
   def take: Async[A] = ???
 
 object QueueSpec extends ZIOSpecDefault:
-  def spec = 
+  def spec =
     suite("QueueSpec")(
       test("make") {
         val queue = Queue.make[Int].block
 
-        /**
-          * EXERCISE 10
-          * 
-          * Implement the `make` method on `Queue` so that it returns an
-          * `Async` that produces a new `Queue`.
+        /** EXERCISE 10
+          *
+          * Implement the `make` method on `Queue` so that it returns an `Async` that produces a new `Queue`.
           */
         assertTrue(queue != null)
       } @@ ignore,
       test("offer + take") {
         val queue = Queue.make[Int].block
 
-        /**
-          * EXERCISE 11
-          * 
-          * Implement the `offer` and `take` methods on `Queue` so that the
-          * `offer` method returns an `Async` that adds an element to the queue,
-          * and the `take` method returns an `Async` that removes an element from
-          * the queue.
+        /** EXERCISE 11
+          *
+          * Implement the `offer` and `take` methods on `Queue` so that the `offer` method returns an `Async` that adds
+          * an element to the queue, and the `take` method returns an `Async` that removes an element from the queue.
           */
         assertTrue(queue.offer(42).fork.flatMap(_ => queue.take).block == 42)
       } @@ ignore,
@@ -243,56 +216,50 @@ object QueueSpec extends ZIOSpecDefault:
 object Semaphore:
   def make(n: Int): Async[Semaphore] = ???
 
-class Semaphore private(val capacity: Int):
+class Semaphore private (val capacity: Int):
   def acquire: Async[Unit] = ???
 
   def release: Async[Unit] = ???
 
 object SemaphoreSpec extends ZIOSpecDefault:
-  def spec = 
+  def spec =
     suite("SemaphoreSpec")(
       test("make") {
         val semaphore = Semaphore.make(1).block
 
-        /**
-          * EXERCISE 12
-          * 
-          * Implement the `make` method on `Semaphore` so that it returns an
-          * `Async` that produces a new `Semaphore` with the specified capacity.
+        /** EXERCISE 12
+          *
+          * Implement the `make` method on `Semaphore` so that it returns an `Async` that produces a new `Semaphore`
+          * with the specified capacity.
           */
         assertTrue(semaphore != null)
       } @@ ignore,
       test("acquire + release") {
         val semaphore = Semaphore.make(1).block
 
-        /**
-          * EXERCISE 13
-          * 
-          * Implement the `acquire` and `release` methods on `Semaphore` so that
-          * the `acquire` method returns an `Async` that blocks until the semaphore
-          * is available, and the `release` method returns an `Async` that releases
-          * the semaphore.
+        /** EXERCISE 13
+          *
+          * Implement the `acquire` and `release` methods on `Semaphore` so that the `acquire` method returns an `Async`
+          * that blocks until the semaphore is available, and the `release` method returns an `Async` that releases the
+          * semaphore.
           */
         assertTrue(semaphore.acquire.fork.flatMap(_ => semaphore.release).block == ())
       } @@ ignore,
       test("concurrent acquire") {
         val semaphore = Semaphore.make(1).block
-       
-        val test = 
+
+        val test =
           for {
             thread1 <- semaphore.acquire.flatMap(_ => semaphore.release).fork
             thread2 <- semaphore.acquire.flatMap(_ => semaphore.release).fork
-            _ <- thread1.join
-            _ <- thread2.join
+            _       <- thread1.join
+            _       <- thread2.join
           } yield 42
 
-        /**
-          * EXERCISE 14
-          * 
-          * Implement the `acquire` method on `Semaphore` so that it can be
-          * acquired concurrently by multiple fibers.
+        /** EXERCISE 14
+          *
+          * Implement the `acquire` method on `Semaphore` so that it can be acquired concurrently by multiple fibers.
           */
         assertTrue(test.block == 42)
       } @@ ignore,
     )
-  
--- src/main/scala/net/degoes/04-failure.scala
+++ src/main/scala/net/degoes/04-failure.scala
@@ -1,29 +1,24 @@
-/**
-  * FAILURE
-  * 
-  * The best effect systems help you reason about failure scenarios, and deal
-  * with them appropriately. Like Rust and Go, effect systems use static types
-  * to help you understand if code can fail, and how.
-  * 
-  * However, functional effect systems like ZIO provide a more nuanced take 
-  * on error handling that recognizes the existence of both recoverable and
-  * unrecoverable errors, and provides a rich set of tools for dealing with
-  * both.
+/** FAILURE
+  *
+  * The best effect systems help you reason about failure scenarios, and deal with them appropriately. Like Rust and Go,
+  * effect systems use static types to help you understand if code can fail, and how.
+  *
+  * However, functional effect systems like ZIO provide a more nuanced take on error handling that recognizes the
+  * existence of both recoverable and unrecoverable errors, and provides a rich set of tools for dealing with both.
   */
-package net.degoes.failure 
+package net.degoes.failure
 
-import zio.test._ 
+import zio.test._
 import zio.test.TestAspect.ignore
 
 object UntypedCheckoutExample:
-  /**
-   * UNTYPED EXAMPLE
-   * 
-   * Reason about the correctness of failure handling in the following code.
-   * 
-   * What are your observations?
-   */
-  def checkout(cart: Cart): Unit = 
+  /** UNTYPED EXAMPLE
+    *
+    * Reason about the correctness of failure handling in the following code.
+    *
+    * What are your observations?
+    */
+  def checkout(cart: Cart): Unit =
     reserveInventory(cart)
     chargeCustomer(cart)
     dispatchOrder(cart)
@@ -42,19 +37,17 @@ object UntypedCheckoutExample:
 
   def sendEmailConfirmation(cart: Cart): Unit = ???
 
-/**
- * TYPED EXAMPLE
- * 
- * This is a checkout example where the errors are all strongly statically typed, 
- * using Scala 3 enums for each class of error, and with clearly distinguished
- * recoverable and unrecoverable errors.
- * 
- * Try to reason about the correctness of the error handling in this code.
- */
+/** TYPED EXAMPLE
+  *
+  * This is a checkout example where the errors are all strongly statically typed, using Scala 3 enums for each class of
+  * error, and with clearly distinguished recoverable and unrecoverable errors.
+  *
+  * Try to reason about the correctness of the error handling in this code.
+  */
 object TypedCheckoutExample:
 
-  def checkout(cart: Cart) = 
-    for 
+  def checkout(cart: Cart) =
+    for
       _ <- reserveInventory(cart)
       _ <- chargeCustomer(cart)
       _ <- dispatchOrder(cart)
@@ -83,18 +76,17 @@ object TypedCheckoutExample:
     case InsufficientCredit
     case AddressVerificationFailed
     case PaymentGatewayDown
-  
+
   enum DispatchError:
     case ServiceDown
-  
+
   enum ShippingLableError:
-    case ServiceDown 
-    case AddressNotFound 
+    case ServiceDown
+    case AddressNotFound
 
   enum EmailSendError:
     case ServiceDown
     case InvalidServer
-
 
 object Fallible:
   def succeed[A](a: A): Fallible[Nothing, A] = ???
@@ -103,16 +95,16 @@ object Fallible:
 
   def attempt[A](f: => A): Fallible[Throwable, A] = ???
 
-  def fromEither[E, A](either: Either[E, A]): Fallible[E, A] = 
+  def fromEither[E, A](either: Either[E, A]): Fallible[E, A] =
     either.fold(fail, succeed)
 
 trait Fallible[+E, +A]:
-  def map[B](f: A => B): Fallible[E, B] = 
+  def map[B](f: A => B): Fallible[E, B] =
     flatMap(a => Fallible.succeed(f(a)))
 
-  def flatMap[E1 >: E, B](f: A => Fallible[E1, B]): Fallible[E1, B] = 
+  def flatMap[E1 >: E, B](f: A => Fallible[E1, B]): Fallible[E1, B] =
     attempt match
-      case Left(e) => Fallible.fail(e)
+      case Left(e)  => Fallible.fail(e)
       case Right(a) => f(a)
 
   def attempt: Either[E, A]
@@ -125,29 +117,26 @@ trait Fallible[+E, +A]:
 
   def refineOrDie[E1](pf: PartialFunction[E, E1])(using ev: E <:< Throwable): Fallible[E1, A] = ???
 
-
 object FallibleSpec extends ZIOSpecDefault:
-  def spec = 
+  def spec =
     suite("FallibleSpec")(
       test("succeed") {
         val result = Fallible.succeed(42)
 
-        /**
-          * EXERCISE 1
-          * 
-          * Implement the `succeed` method on `Fallible` so that it returns a
-          * `Fallible` that succeeds with the specified value.
+        /** EXERCISE 1
+          *
+          * Implement the `succeed` method on `Fallible` so that it returns a `Fallible` that succeeds with the
+          * specified value.
           */
         assertTrue(result.attempt == Right(42))
       } @@ ignore,
       test("fail") {
         val result = Fallible.fail("Uh oh")
 
-        /**
-          * EXERCISE 2
-          * 
-          * Implement the `fail` method on `Fallible` so that it returns a
-          * `Fallible` that fails with the specified error.
+        /** EXERCISE 2
+          *
+          * Implement the `fail` method on `Fallible` so that it returns a `Fallible` that fails with the specified
+          * error.
           */
         assertTrue(result.attempt == Left("Uh oh"))
       } @@ ignore,
@@ -155,99 +144,83 @@ object FallibleSpec extends ZIOSpecDefault:
         val success = Fallible.attempt(42)
         val failure = Fallible.attempt(throw new Error("Uh oh"))
 
-        /**
-          * EXERCISE 3
-          * 
-          * Implement the `attempt` method on `Fallible` so that it returns a
-          * `Fallible` that succeeds with the value produced by the specified
-          * effect, or fails with the error produced by the effect.
+        /** EXERCISE 3
+          *
+          * Implement the `attempt` method on `Fallible` so that it returns a `Fallible` that succeeds with the value
+          * produced by the specified effect, or fails with the error produced by the effect.
           */
-        assertTrue(success.attempt == Right(42)) && 
+        assertTrue(success.attempt == Right(42)) &&
         assertTrue(failure.attempt.isLeft)
       } @@ ignore,
       test("fromEither") {
         val result = Fallible.fromEither(Right(42))
 
-        /**
-          * EXERCISE 4 
-          * 
-          * Implement the `fromEither` method on `Fallible` so that it returns a
-          * `Fallible` that succeeds with the value produced by the specified
-          * `Either`, or fails with the error produced by the `Either`.
+        /** EXERCISE 4
+          *
+          * Implement the `fromEither` method on `Fallible` so that it returns a `Fallible` that succeeds with the value
+          * produced by the specified `Either`, or fails with the error produced by the `Either`.
           */
         assertTrue(result.attempt == Right(42))
       } @@ ignore,
       test("map") {
         val result = Fallible.succeed(42).map(_ + 1)
 
-        /**
-          * EXERCISE 5
-          * 
-          * Implement the `map` method on `Fallible` so that it returns a
-          * `Fallible` that applies the specified function to the value produced
-          * by the source `Fallible`.
+        /** EXERCISE 5
+          *
+          * Implement the `map` method on `Fallible` so that it returns a `Fallible` that applies the specified function
+          * to the value produced by the source `Fallible`.
           */
         assertTrue(result.attempt == Right(43))
       } @@ ignore,
       test("flatMap") {
         val result = Fallible.succeed(42).flatMap(a => Fallible.succeed(a + 1))
 
-        /**
-          * EXERCISE 6
-          * 
-          * Implement the `flatMap` method on `Fallible` so that it returns a
-          * `Fallible` that applies the specified function to the value produced
-          * by the source `Fallible`.
+        /** EXERCISE 6
+          *
+          * Implement the `flatMap` method on `Fallible` so that it returns a `Fallible` that applies the specified
+          * function to the value produced by the source `Fallible`.
           */
         assertTrue(result.attempt == Right(43))
       } @@ ignore,
       test("get") {
         val result = Fallible.succeed(42)
 
-        /**
-          * EXERCISE 7
-          * 
-          * Implement the `get` method on `Fallible` so that it returns the value
-          * produced by the `Fallible`, or throws an exception if the `Fallible`
-          * failed.
+        /** EXERCISE 7
+          *
+          * Implement the `get` method on `Fallible` so that it returns the value produced by the `Fallible`, or throws
+          * an exception if the `Fallible` failed.
           */
         assertTrue(result.get == 42)
       } @@ ignore,
       test("orDie") {
         val result = Fallible.succeed(42)
 
-        /**
-          * EXERCISE 8
-          * 
-          * Implement the `orDie` method on `Fallible` so that it returns a
-          * `Fallible` that succeeds with the value produced by the source
-          * `Fallible`, or fails with the error produced by the source `Fallible`.
+        /** EXERCISE 8
+          *
+          * Implement the `orDie` method on `Fallible` so that it returns a `Fallible` that succeeds with the value
+          * produced by the source `Fallible`, or fails with the error produced by the source `Fallible`.
           */
         assertTrue(result.orDie.attempt == Right(42))
       } @@ ignore,
       test("mapError") {
         val result = Fallible.fail("Uh oh").mapError(_.length)
 
-        /**
-          * EXERCISE 9
-          * 
-          * Implement the `mapError` method on `Fallible` so that it returns a
-          * `Fallible` that applies the specified function to the error produced
-          * by the source `Fallible`.
+        /** EXERCISE 9
+          *
+          * Implement the `mapError` method on `Fallible` so that it returns a `Fallible` that applies the specified
+          * function to the error produced by the source `Fallible`.
           */
         assertTrue(result.attempt == Left(5))
       } @@ ignore,
       test("refineOrDie") {
         val result = Fallible.fail(new Error).refineOrDie { case _ => 42 }
 
-        /**
-          * EXERCISE 10
-          * 
-          * Implement the `refineOrDie` method on `Fallible` so that it returns a
-          * `Fallible` that succeeds with the value produced by the source `Fallible`,
-          * or fails with the error produced by the source `Fallible`, refined by the
-          * specified partial function (by refining, it is meant that if the error doesn't 
-          * match the refined type, it will be thrown as an exception).
+        /** EXERCISE 10
+          *
+          * Implement the `refineOrDie` method on `Fallible` so that it returns a `Fallible` that succeeds with the
+          * value produced by the source `Fallible`, or fails with the error produced by the source `Fallible`, refined
+          * by the specified partial function (by refining, it is meant that if the error doesn't match the refined
+          * type, it will be thrown as an exception).
           */
         assertTrue(result.attempt.isLeft)
       } @@ ignore,
